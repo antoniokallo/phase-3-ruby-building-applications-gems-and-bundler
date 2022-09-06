930f6748b0c367727bbc9aba87b02a2bd76a5297
antoniokallo <antoniokallo@gmail.com>
2022-09-06 16:04:53 -0400

    commit tree 34782f429b0da06cb0f5448d256522e4ec7e0126

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,9 @@
-# require the environment file
+# require the environment filesource "https://rubygems.org"
+gem "rspec"
+gem "rest-client"
+
+group :development do
+  gem "pry"
+end
+
+require_relative '../config/environment'
